14175223832782e5a0905a6c8078760448313481
jacobharraldson <jharraldson@gmail.com>
2023-09-30 11:00:00 +0200

    local storage saving added + single task styling updated

--- app.js
+++ app.js
@@ -9,32 +9,47 @@ function addTask(e) {
         alert('Please provide task name!')
     } else {
         const li = document.createElement('li');
-        li.textContent = taskInput.value;
+        const para = document.createElement('p')
+        para.textContent = taskInput.value;
+        para.classList.add('todo__singleTaskWrapper');
         li.classList.add('todo__newTask')
         const checkTask = document.createElement('span');
         checkTask.classList.add('todo__checkTask', 'fa-regular', 'fa-circle')
         const remove = document.createElement('span')
         remove.classList.add('todo__removeTaskBtn', 'fa-solid', 'fa-circle-xmark');
 
+        li.appendChild(para)
         li.appendChild(checkTask)
         li.appendChild(remove);
         taskContainer.appendChild(li);
     }
 
     taskInput.value = '';
+    saveTasks();
 }
 
 taskContainer.addEventListener('click', (e) => {
     if (e.target.classList.contains('todo__checkTask')) {
         e.target.classList.toggle("fa-circle-check");
         e.target.parentElement.classList.toggle('todo__textThrough');
+        saveTasks()
 
 
     } else if (e.target.classList.contains('todo__removeTaskBtn')) {
         e.target.parentElement.remove();
+        saveTasks()
     }
 })
 
-
 addTaskBtn.addEventListener('click', addTask);
 
+function saveTasks() {
+    localStorage.setItem('data', taskContainer.innerHTML);
+}
+
+
+function showTasks() {
+    taskContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTasks();
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -11,26 +11,42 @@ function addTask(e) {
     alert('Please provide task name!');
   } else {
     var li = document.createElement('li');
-    li.textContent = taskInput.value;
+    var para = document.createElement('p');
+    para.textContent = taskInput.value;
+    para.classList.add('todo__singleTaskWrapper');
     li.classList.add('todo__newTask');
     var checkTask = document.createElement('span');
     checkTask.classList.add('todo__checkTask', 'fa-regular', 'fa-circle');
     var remove = document.createElement('span');
     remove.classList.add('todo__removeTaskBtn', 'fa-solid', 'fa-circle-xmark');
+    li.appendChild(para);
     li.appendChild(checkTask);
     li.appendChild(remove);
     taskContainer.appendChild(li);
   }
 
   taskInput.value = '';
+  saveTasks();
 }
 
 taskContainer.addEventListener('click', function (e) {
   if (e.target.classList.contains('todo__checkTask')) {
     e.target.classList.toggle("fa-circle-check");
     e.target.parentElement.classList.toggle('todo__textThrough');
+    saveTasks();
   } else if (e.target.classList.contains('todo__removeTaskBtn')) {
     e.target.parentElement.remove();
+    saveTasks();
   }
 });
 addTaskBtn.addEventListener('click', addTask);
+
+function saveTasks() {
+  localStorage.setItem('data', taskContainer.innerHTML);
+}
+
+function showTasks() {
+  taskContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTasks();
